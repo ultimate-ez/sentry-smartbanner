9ac6a8626ed615d2f7b48389699ac9e34cd1c4d4
MasEz <ultimateez@gmail.com>
2017-05-24 14:11:17 +0900

    [add]メインファイル作成
    プラグインのメインファイルを作成

---
+++ sentry-smartbanner.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Sentry SmartBanner
+Plugin URI:
+Description: Sentry SmartBannerは汎用のバナーをページに表示するためのプラグインです。
+Version: 1.0.0
+Author: Masashi Ezaki
+Autohr URI: https://ultimate-ez.com
+License: GPL2
+*/
+?>
