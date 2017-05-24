bae816ac83b5fdba84fc1c08d62e8cec78569ad1
MasEz <ultimateez@gmail.com>
2017-05-24 14:32:33 +0900

    [update]footerに文字列を表示
    echoで固定文字列を直接表示させてみる

--- sentry-smartbanner.php
+++ sentry-smartbanner.php
@@ -8,4 +8,11 @@ Author: Masashi Ezaki
 Autohr URI: https://ultimate-ez.com
 License: GPL2
 */
+
+add_action('wp_footer', 'helloworld');
+
+function helloworld(){
+  echo "--- Hello world ---";
+}
+
 ?>
