8a5d218e32123a5786aee1a76230f4d87c62a4e2
MasEz <ultimateez@gmail.com>
2017-05-31 16:18:29 +0900

    [add]CSSファイルの追加
    プラグイン用のCSSを追加。class内でwp_enqueue_styleで読み込む

--- sentry-smartbanner.php
+++ sentry-smartbanner.php
@@ -12,7 +12,14 @@ License: GPL2
 class Sentry_SmartBanner {
 
   public function __construct() {
+    add_action( 'init', array( $this, 'enqueue_scripts_and_styles' ) );
+
     add_action('wp_footer', array( $this, 'helloworld' ) );
+  }
+
+  public function enqueue_scripts_and_styles() {
+    //css
+		wp_enqueue_style( 'sentry_smartbanner', plugins_url( 'sentry-smartbanner.css' , __FILE__ ) );
   }
 
   public function helloworld(){
